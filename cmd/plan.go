fc1ec06633d45a8ff019809dd274f25e4fd9cbfb
Gu1llaum-3 <archambault.guillaume06@gmail.com>
2024-07-13 17:49:58 +0200

    Add --ssh-key in plan output

--- cmd/plan.go
+++ cmd/plan.go
@@ -1,21 +1,21 @@
 package cmd
 
 import (
-	"encoding/json"
-	"fmt"
-	"os"
+  "encoding/json"
+  "fmt"
+  "os"
 
-	"github.com/alexellis/k3sup/pkg"
-	"github.com/spf13/cobra"
+  "github.com/alexellis/k3sup/pkg"
+  "github.com/spf13/cobra"
 )
 
 func MakePlan() *cobra.Command {
-	var initFlag bool
+  var initFlag bool
 
-	var command = &cobra.Command{
-		Use:   "plan",
-		Short: "Plan an installation of K3s.",
-		Long: `Generate a bash script or plan of installation commands for K3s for a 
+  var command = &cobra.Command{
+    Use:   "plan",
+    Short: "Plan an installation of K3s.",
+    Long: `Generate a bash script or plan of installation commands for K3s for a
 Highly Available (HA) Kubernetes cluster.
 
 Examples JSON input file:
@@ -26,7 +26,7 @@ Examples JSON input file:
 
 ` + pkg.SupportMessageShort + `
 `,
-		Example: `  # Generate an installation script where the first
+    Example: `  # Generate an installation script where the first
   # 3 available hosts are dedicated as servers, with a custom user.
   # The remaining hosts are added as agents.
   k3sup plan hosts.json --servers 3 --user ubuntu
@@ -37,163 +37,170 @@ Examples JSON input file:
   # Create an example hosts.json file
   k3sup plan --init
 `,
-		SilenceUsage: true,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			if initFlag {
-				return outputExampleHostsJSON()
-			}
+    SilenceUsage: true,
+    RunE: func(cmd *cobra.Command, args []string) error {
+      if initFlag {
+        return outputExampleHostsJSON()
+      }
 
-			if len(args) == 0 {
-				return fmt.Errorf("give a path to a JSON file containing a list of devices")
-			}
+      if len(args) == 0 {
+        return fmt.Errorf("give a path to a JSON file containing a list of devices")
+      }
 
-			nodeLimit, _ := cmd.Flags().GetInt("limit")
-			name := args[0]
-			data, err := os.ReadFile(name)
-			if err != nil {
-				return err
-			}
+      nodeLimit, _ := cmd.Flags().GetInt("limit")
+      name := args[0]
+      data, err := os.ReadFile(name)
+      if err != nil {
+        return err
+      }
 
-			background, _ := cmd.Flags().GetBool("background")
+      background, _ := cmd.Flags().GetBool("background")
 
-			var hosts []Host
-			if err = json.Unmarshal(data, &hosts); err != nil {
-				return err
-			}
+      var hosts []Host
+      if err = json.Unmarshal(data, &hosts); err != nil {
+        return err
+      }
 
-			serverK3sExtraArgs, _ := cmd.Flags().GetString("server-k3s-extra-args")
-			agentK3sExtraArgs, _ := cmd.Flags().GetString("agent-k3s-extra-args")
+      serverK3sExtraArgs, _ := cmd.Flags().GetString("server-k3s-extra-args")
+      agentK3sExtraArgs, _ := cmd.Flags().GetString("agent-k3s-extra-args")
 
-			servers, _ := cmd.Flags().GetInt("servers")
-			kubeconfig, _ := cmd.Flags().GetString("local-path")
-			contextName, _ := cmd.Flags().GetString("context")
-			user, _ := cmd.Flags().GetString("user")
-			tlsSan, _ := cmd.Flags().GetString("tls-san")
+      servers, _ := cmd.Flags().GetInt("servers")
+      kubeconfig, _ := cmd.Flags().GetString("local-path")
+      contextName, _ := cmd.Flags().GetString("context")
+      user, _ := cmd.Flags().GetString("user")
+      sshKey, _ := cmd.Flags().GetString("ssh-key")
+      tlsSan, _ := cmd.Flags().GetString("tls-san")
 
-			tlsSanStr := ""
-			if len(tlsSan) > 0 {
-				tlsSanStr = fmt.Sprintf(` \
+      tlsSanStr := ""
+      if len(tlsSan) > 0 {
+        tlsSanStr = fmt.Sprintf(` \
 --tls-san %s`, tlsSan)
-			}
-			// sshKey, _ := cmd.Flags().GetString("ssh-key")
+      }
 
-			bgStr := ""
-			if background {
-				bgStr = " &"
-			}
+      sshKeyStr := ""
+      if len(sshKey) > 0 {
+        sshKeyStr = fmt.Sprintf(` \
+--ssh-key %s`, sshKey)
+      }
 
-			serversAdded := 0
-			var primaryServer Host
-			script := "#!/bin/sh\n\n"
+      bgStr := ""
+      if background {
+        bgStr = " &"
+      }
 
-			serverExtraArgsSt := ""
-			if len(serverK3sExtraArgs) > 0 {
-				serverExtraArgsSt = fmt.Sprintf(` \
+      serversAdded := 0
+      var primaryServer Host
+      script := "#!/bin/sh\n\n"
+
+      serverExtraArgsSt := ""
+      if len(serverK3sExtraArgs) > 0 {
+        serverExtraArgsSt = fmt.Sprintf(` \
 --k3s-extra-args "%s"`, serverK3sExtraArgs)
-			}
-			agentExtraArgsSt := ""
-			if len(agentK3sExtraArgs) > 0 {
-				agentExtraArgsSt = fmt.Sprintf(` \
+      }
+      agentExtraArgsSt := ""
+      if len(agentK3sExtraArgs) > 0 {
+        agentExtraArgsSt = fmt.Sprintf(` \
 --k3s-extra-args "%s"`, agentK3sExtraArgs)
-			}
+      }
 
-			for i, host := range hosts {
-				if serversAdded == 0 {
+      for i, host := range hosts {
+        if serversAdded == 0 {
 
-					script += `echo "Setting up primary server 1"
+          script += `echo "Setting up primary server 1"
 `
 
-					script += fmt.Sprintf(`k3sup install --host %s \
---user %s \
+          script += fmt.Sprintf(`k3sup install --host %s \
+--user %s%s \
 --cluster \
 --local-path %s \
 --context %s%s%s
 `,
-						host.IP,
-						user,
-						kubeconfig,
-						contextName,
-						tlsSanStr,
-						serverExtraArgsSt)
+            host.IP,
+            user,
+            sshKeyStr,
+            kubeconfig,
+            contextName,
+            tlsSanStr,
+            serverExtraArgsSt)
 
-					script += fmt.Sprintf(`
+          script += fmt.Sprintf(`
 echo "Fetching the server's node-token into memory"
 
-export NODE_TOKEN=$(k3sup node-token --host %s --user %s)
-`, host.IP, user)
+export NODE_TOKEN=$(k3sup node-token --host %s --user %s%s)
+`, host.IP, user, sshKeyStr)
 
-					serversAdded = 1
-					primaryServer = host
-				} else if serversAdded < servers {
-					script += fmt.Sprintf("\necho \"Setting up additional server: %d\"\n", serversAdded+1)
+          serversAdded = 1
+          primaryServer = host
+        } else if serversAdded < servers {
+          script += fmt.Sprintf("\necho \"Setting up additional server: %d\"\n", serversAdded+1)
 
-					script += fmt.Sprintf(`k3sup join \
+          script += fmt.Sprintf(`k3sup join \
 --host %s \
 --server-host %s \
 --server \
 --node-token "$NODE_TOKEN" \
---user %s%s%s%s
-`, host.IP, primaryServer.IP, user, tlsSanStr, serverExtraArgsSt, bgStr)
+--user %s%s%s%s%s
+`, host.IP, primaryServer.IP, user, sshKeyStr, tlsSanStr, serverExtraArgsSt, bgStr)
 
-					serversAdded++
-				} else {
-					script += fmt.Sprintf("\necho \"Setting up worker: %d\"\n", (i+1)-serversAdded)
+          serversAdded++
+        } else {
+          script += fmt.Sprintf("\necho \"Setting up worker: %d\"\n", (i+1)-serversAdded)
 
-					script += fmt.Sprintf(`k3sup join \
+          script += fmt.Sprintf(`k3sup join \
 --host %s \
 --server-host %s \
 --node-token "$NODE_TOKEN" \
---user %s%s%s
-`, host.IP, primaryServer.IP, user, agentExtraArgsSt, bgStr)
-				}
+--user %s%s%s%s
+`, host.IP, primaryServer.IP, user, sshKeyStr, agentExtraArgsSt, bgStr)
+        }
 
-				if nodeLimit > 0 && i+1 >= nodeLimit {
-					break
-				}
-			}
+        if nodeLimit > 0 && i+1 >= nodeLimit {
+          break
+        }
+      }
 
-			fmt.Printf("%s\n", script)
+      fmt.Printf("%s\n", script)
 
-			return nil
-		},
-	}
+      return nil
+    },
+  }
 
-	command.Flags().Int("servers", 3, "Number of servers to use from the devices file")
-	command.Flags().String("local-path", "kubeconfig", "Where to save the kubeconfig file")
-	command.Flags().String("context", "default", "Name of the kubeconfig context to use")
-	command.Flags().String("user", "root", "Username for SSH login")
+  command.Flags().Int("servers", 3, "Number of servers to use from the devices file")
+  command.Flags().String("local-path", "kubeconfig", "Where to save the kubeconfig file")
+  command.Flags().String("context", "default", "Name of the kubeconfig context to use")
+  command.Flags().String("user", "root", "Username for SSH login")
 
-	command.Flags().String("ssh-key", "", "Path to the private key for SSH login")
-	command.Flags().String("tls-san", "", "SAN for TLS certificates, can be a comma-separated list")
-	command.Flags().String("server-k3s-extra-args", "", "Extra arguments to be passed into the k3s server")
-	command.Flags().String("agent-k3s-extra-args", "", "Extra arguments to be passed into the k3s agent")
+  command.Flags().String("ssh-key", "", "Path to the private key for SSH login")
+  command.Flags().String("tls-san", "", "SAN for TLS certificates, can be a comma-separated list")
+  command.Flags().String("server-k3s-extra-args", "", "Extra arguments to be passed into the k3s server")
+  command.Flags().String("agent-k3s-extra-args", "", "Extra arguments to be passed into the k3s agent")
 
-	// Background
-	command.Flags().Bool("background", false, "Run the installation in the background for all agents/nodes after the first server is up")
+  // Background
+  command.Flags().Bool("background", false, "Run the installation in the background for all agents/nodes after the first server is up")
 
-	command.Flags().Int("limit", 0, "Maximum number of nodes to use from the devices file, 0 to use all devices")
-	command.Flags().BoolVar(&initFlag, "init", false, "Output an example hosts.json file")
+  command.Flags().Int("limit", 0, "Maximum number of nodes to use from the devices file, 0 to use all devices")
+  command.Flags().BoolVar(&initFlag, "init", false, "Output an example hosts.json file")
 
-	return command
+  return command
 }
 
 func outputExampleHostsJSON() error {
-	exampleHosts := []Host{
-		{Hostname: "node-1", IP: "192.168.128.102"},
-		{Hostname: "node-2", IP: "192.168.128.103"},
-		{Hostname: "node-3", IP: "192.168.128.104"},
-	}
+  exampleHosts := []Host{
+    {Hostname: "node-1", IP: "192.168.128.102"},
+    {Hostname: "node-2", IP: "192.168.128.103"},
+    {Hostname: "node-3", IP: "192.168.128.104"},
+  }
 
-	data, err := json.MarshalIndent(exampleHosts, "", "  ")
-	if err != nil {
-		return fmt.Errorf("could not encode JSON: %v", err)
-	}
+  data, err := json.MarshalIndent(exampleHosts, "", "  ")
+  if err != nil {
+    return fmt.Errorf("could not encode JSON: %v", err)
+  }
 
-	fmt.Println(string(data))
-	return nil
+  fmt.Println(string(data))
+  return nil
 }
 
 type Host struct {
-	Hostname string `json:"hostname"`
-	IP       string `json:"ip"`
+  Hostname string `json:"hostname"`
+  IP       string `json:"ip"`
 }
